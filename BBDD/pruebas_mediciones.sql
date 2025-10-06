d424007c6d4f20316fa43a4a675d63ff5d5515c6
Caronte2 <147274913+Caronte2@users.noreply.github.com>
2025-10-06 23:27:15 +0200

    primeras pruebas base de datos, apir rest, y logica de negocio
    Este commit es de los primeros en el que pretendo declarar los metodos de la logica del negicio, la api para hacer la conexiones y la base de datos con el que trabajare. Para ello la base de datos lo estamos haciendo en phpMyAdmin y tanto la logica como la api en php

---
+++ BBDD/pruebas_mediciones.sql
@@ -0,0 +1,85 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-10-2025 a las 23:22:00
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pruebas_mediciones`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mediciones`
+--
+
+CREATE TABLE `mediciones` (
+  `id_medicion` int(11) NOT NULL,
+  `id_sensor` int(11) NOT NULL,
+  `C02` float NOT NULL,
+  `Temperatura` float NOT NULL,
+  `Fecha` date NOT NULL,
+  `Hora` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sensor`
+--
+
+CREATE TABLE `sensor` (
+  `id_sensor` int(11) NOT NULL,
+  `Mac` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `mediciones`
+--
+ALTER TABLE `mediciones`
+  ADD PRIMARY KEY (`id_medicion`);
+
+--
+-- Indices de la tabla `sensor`
+--
+ALTER TABLE `sensor`
+  ADD PRIMARY KEY (`id_sensor`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `mediciones`
+--
+ALTER TABLE `mediciones`
+  MODIFY `id_medicion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `sensor`
+--
+ALTER TABLE `sensor`
+  MODIFY `id_sensor` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
